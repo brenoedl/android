5e4d79149731b49ab6c11e3edc5d935e6524ae21
Breno Emanuel Dantas de Lira <96654144+brenoedl@users.noreply.github.com>
2025-03-22 22:48:54 -0300

    Update FormCadatro.kt

--- xml/Kotlin/AppDeFilme/app/src/main/java/com/brenoedll/appdefilme/FormCadatro.kt
+++ xml/Kotlin/AppDeFilme/app/src/main/java/com/brenoedll/appdefilme/FormCadatro.kt
@@ -6,6 +6,10 @@ import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.brenoedll.appdefilme.databinding.ActivityFormCadatroBinding
+import com.google.firebase.FirebaseNetworkException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthUserCollisionException
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException
 
 class FormCadatro : AppCompatActivity() {
     private lateinit var binding: ActivityFormCadatroBinding
@@ -41,13 +45,9 @@ class FormCadatro : AppCompatActivity() {
             val email = binding.tietEmailCadastro.text.toString().trim()
             val senha = binding.tietSenhaCadastro.text.toString()
 
-            if (email.isNotEmpty() && senha.isNotEmpty()){
-                Toast.makeText(this, getString(R.string.msg_sucesso_cadastro), Toast.LENGTH_SHORT).show()
-                binding.tilEmailCadastro.helperText = ""
-                binding.tilEmailCadastro.boxStrokeColor = getColor(R.color.blue)
-                binding.tilSenhaCadastro.helperText = ""
-                binding.tilSenhaCadastro.boxStrokeColor = getColor(R.color.blue)
-            }else if (senha.isEmpty()) {
+            if (email.isNotEmpty() && senha.isNotEmpty()) {
+                cadastro(email, senha)
+            } else if (senha.isEmpty()) {
                 binding.tilSenhaCadastro.helperText = getString(R.string.erro_senha1)
                 binding.tilSenhaCadastro.boxStrokeColor = getColor(R.color.red)
                 binding.tilEmailCadastro.boxStrokeColor = getColor(R.color.blue)
@@ -60,8 +60,39 @@ class FormCadatro : AppCompatActivity() {
         }
 
         binding.tvEntrar.setOnClickListener {
-            val  intent = Intent(this, FormLogin::class.java)
+            val intent = Intent(this, FormLogin::class.java)
             startActivity(intent)
+        }
+    }
+
+    private fun cadastro(email: String, senha: String) {
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, senha).addOnCompleteListener { cadastro ->
+            if (cadastro.isSuccessful) {
+                Toast.makeText(this, getString(R.string.msg_sucesso_cadastro), Toast.LENGTH_SHORT).show()
+                binding.tilEmailCadastro.helperText = ""
+                binding.tilEmailCadastro.boxStrokeColor = getColor(R.color.blue)
+                binding.tilSenhaCadastro.helperText = ""
+                binding.tilSenhaCadastro.boxStrokeColor = getColor(R.color.blue)
+            }
+        }.addOnFailureListener { exception ->
+
+            val erro = exception
+            when {
+                erro is FirebaseAuthWeakPasswordException -> {
+                    binding.tilSenhaCadastro.helperText = getString(R.string.erro_senha2)
+                    binding.tilSenhaCadastro.boxStrokeColor = getColor(R.color.red)
+                }
+                erro is FirebaseAuthUserCollisionException -> {
+                    binding.tilEmailCadastro.helperText = getString(R.string.erro_conta)
+                    binding.tilEmailCadastro.boxStrokeColor = getColor(R.color.red)
+                }
+                erro is FirebaseNetworkException -> {
+                    Toast.makeText(this, getString(R.string.erro_internet), Toast.LENGTH_SHORT).show()
+                }
+                else -> {
+                    Toast.makeText(this, getString(R.string.msg_erro_cadastro), Toast.LENGTH_SHORT).show()
+                }
+            }
         }
     }
 }
