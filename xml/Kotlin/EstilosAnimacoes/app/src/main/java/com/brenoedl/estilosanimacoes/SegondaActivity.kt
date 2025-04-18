cd37b32f04a6aa1b6474933d8efc78bec61ec555
Breno Emanuel Dantas de Lira <96654144+brenoedl@users.noreply.github.com>
2025-04-17 23:32:04 -0300

    Estilos Animações

---
+++ xml/Kotlin/EstilosAnimacoes/app/src/main/java/com/brenoedl/estilosanimacoes/SegondaActivity.kt
@@ -0,0 +1,14 @@
+package com.brenoedl.estilosanimacoes
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SegondaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_segonda)
+    }
+}
