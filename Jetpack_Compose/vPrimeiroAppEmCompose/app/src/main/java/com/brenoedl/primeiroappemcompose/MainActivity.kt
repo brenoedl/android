722a24f2181493d2a7619e96b05c650e139533e0
Breno Emanuel Dantas de Lira <96654144+brenoedl@users.noreply.github.com>
2025-06-06 22:06:55 -0300

    Primeiro App Em Compose

---
+++ Jetpack_Compose/vPrimeiroAppEmCompose/app/src/main/java/com/brenoedl/primeiroappemcompose/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.brenoedl.primeiroappemcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.snapping.SnapPosition
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.brenoedl.primeiroappemcompose.ui.theme.PrimeiroAppEmComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PrimeiroAppEmComposeTheme {
+                Home()
+            }
+        }
+    }
+}
+
+@Composable
+fun Home() {
+    Column(
+        modifier = Modifier.fillMaxSize().background(Color.White),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text("Olá Mundo!", fontSize = 24.sp, color = Color.Black)
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HomePreview() {
+    PrimeiroAppEmComposeTheme {
+        Home()
+    }
+}
