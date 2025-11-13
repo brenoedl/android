87bb5c9ad31cc7c6f031ce29b5ef70aef592201d
Breno Emanuel Dantas de Lira <96654144+brenoedl@users.noreply.github.com>
2025-11-13 17:46:05 -0300

    hhb n

--- Jetpack_Compose/AppDeIdiomas/app/src/main/java/com/brenoedl/appdeidiomas/MainActivity.kt
+++ Jetpack_Compose/AppDeIdiomas/app/src/main/java/com/brenoedl/appdeidiomas/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -32,10 +33,12 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.brenoedl.appdeidiomas.ui.theme.AppDeIdiomasTheme
+import androidx.compose.ui.unit.sp
 import com.brenoedl.appdeidiomas.ui.theme.Blue900
+import com.brenoedl.appdeidiomas.ui.theme.Gray900
 import com.brenoedl.appdeidiomas.ui.theme.White
 
 class MainActivity : ComponentActivity() {
@@ -143,14 +146,26 @@ fun Home(){
                 contentDescription = null,
                 modifier = Modifier.size(200.dp)
             )
+
+            Text(
+                text = stringResource(id = R.string.idioma),
+                fontSize = 40.sp,
+                color = Gray900,
+                fontWeight = FontWeight.Bold
+            )
+
+            Text(
+                text = stringResource(id = R.string.descricao),
+                fontSize = 20.sp,
+                modifier = Modifier.padding(20.dp).align(Alignment.CenterHorizontally)
+                    .border(2.dp, Blue900).padding(10.dp)
+            )
         }
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    AppDeIdiomasTheme {
+fun HomePreview() {
         Home()
-    }
 }
