d04f72c43b78948ca40482355ed734e847535fb5
Breno Emanuel Dantas de Lira <96654144+brenoedl@users.noreply.github.com>
2025-11-24 19:04:38 -0300

    Update OutlinedTextFieldCustom.kt

--- Jetpack_Compose/Agenda De Contatos Em Compose/app/src/main/java/com/brenoedl/agendadecontatosemcompose/componentes/OutlinedTextFieldCustom.kt
+++ Jetpack_Compose/Agenda De Contatos Em Compose/app/src/main/java/com/brenoedl/agendadecontatosemcompose/componentes/OutlinedTextFieldCustom.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -42,5 +41,4 @@ fun OutlinedTextFieldCustomPreview(){
         label = { Text("Nome") },
         keyboardOptions = KeyboardOptions.Default
     )
-
 }
